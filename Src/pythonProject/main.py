b792dfc1a68abf3d63b654c7af9091191d25134d
Mikel Mangion <73097576+ElementalBread@users.noreply.github.com>
2021-05-14 10:35:23 +0200

    Algorithm update
    An update on the algorithm

--- Src/pythonProject/main.py
+++ Src/pythonProject/main.py
@@ -125,44 +125,15 @@ def translateTextToBraille(textToTranslate):
     return translatedText
 
 def translateBrailleToText(textToTranslate):
-    dictionary = {"⠠⠁": 65,  # A⠁
-                  '⠠⠃': 66,  # B
-                  '⠠⠉': 266,  # Ċ
-                  '⠠⠙': 68,  # D
-                  '⠠⠑': 69,  # E
-                  '⠠⠋': 70,  # F
-                  '⠠⠾': 288,   # Ġ
-                  '⠠⠛': 71,  # G
-                  '⠠⠓': 72,  # H
-                  '⠠⠮': 294,  # Ħ
-                  '⠠⠊': 73,  # I
-                  '⠠⠚': 74,  # J
-                  '⠠⠅': 75,  # K
-                  '⠠⠇': 76,  # L
-                  77: '⠠⠍',  # M
-                  78: '⠠⠝',  # N
-                  79: '⠠⠕',  # O
-                  80: '⠠⠏',  # P
-                  81: '⠠⠟',  # Q
-                  82: '⠠⠗',   # R
-                  83: '⠠⠎',  # S
-                  84: '⠠⠞',  # T
-                  85: '⠠⠥',  # U
-                  86: '⠠⠧',  # V
-                  87: '⠠⠺',  # W
-                  88: '⠠⠭',  # X
-                  379: '⠠⠽',  # Ż
-                  90: '⠠⠵',  # Z
-                  97: '⠁',  # a
-                  98: '⠃',  # b
-                  99: '⠉',  # c
-                  267: '⠉',  # ċ
-                  100: '⠙ ',  # d
-                  101: '⠑',  # e
-                  102: '⠋',  # f
-                  289: '⠾',  # ġ
-                  103: '⠛',  # g
-                  104: '⠓',  # h
+    dictionary = {10241: 97 ,  # ⠁ a
+                  '': 98 ,  # ⠃b
+                  '': 267 ,  # ⠉ ċ
+                  100: '',  # ⠙ d
+                  101: '',  # ⠑ e
+                  102: '',  # ⠋ f
+                  289: '',  # ⠾ ġ
+                  103: '',  # ⠛ g
+                  104: '',  # ⠓ h
                   295: '⠮',  # ħ
                   105: '⠊',  # i
                   106: '⠚',  # j
@@ -201,40 +172,32 @@ def translateBrailleToText(textToTranslate):
                   63: '⠦',  # ?
                   8220: '⠘⠴',  # “ (close)
                   8221: '⠘⠦',  # ” (open)
-                  8216: '⠄⠴', # ' (close)
-                  8217: '⠄⠦', # ' (open)
-                  40: '⠐⠣', # (
-                  41: '⠐⠜', # )
-                  47: '⠸⠌', # /
-                  92: '⠸⠡', # \
+                  8216: '⠄⠴',  # ' (close)
+                  8217: '⠄⠦',  # ' (open)
+                  40: '⠐⠣',  # (
+                  41: '⠐⠜',  # )
+                  47: '⠸⠌',  # /
+                  92: '⠸⠡',  # \
                   }
 
     translatedText = ""
     numberCache = ""
+    letterCapital = False
     i = 0
     for letter in textToTranslate:
-        if letter.isnumeric():
-            numberCache = numberCache + letter
-        elif letter == 'g':
-            if (textToTranslate[i+1] == 'ħ'):
-                translatedText = translatedText + '⠣'
-        elif letter == 'G':
-            if (textToTranslate[i+1] == 'ħ'):
-                translatedText = translatedText + '⠣'
-        elif letter == 'ħ':
-            if ((textToTranslate[i-1] != 'g') & (textToTranslate[i-1] != 'G')):
-                letter = letter.translate(dictionary)
-                translatedText = translatedText + letter
+        if letter == '⠠':
+            letterCapital = True
         else:
             if numberCache != '':
-                numberCache = '⠼' + numberCache.translate(dictionary)
-                translatedText = translatedText + numberCache + letter.translate(dictionary)
-                numberCache = ''
+                print("Number cache full")
             else:
                 if letter == ' ':
                     translatedText = translatedText + ' '
                 else:
                     letter = letter.translate(dictionary)
+                    if letterCapital == True:
+                        letter = letter.upper()
+                        letterCapital = False
                     translatedText = translatedText + letter
 
         i = i + 1
